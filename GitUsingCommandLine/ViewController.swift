e350bcc529acfbbb294d49cac42c2cedb178fa0a
Apple <apple@apples-macbook-pro.local>
2018-12-05 16:40:19 +0530

    Initial Commit

---
+++ GitUsingCommandLine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUsingCommandLine
+//
+//  Created by Apple on 05/12/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
